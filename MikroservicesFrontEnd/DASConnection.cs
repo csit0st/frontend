3207b06b20f41ee9b43530a54ec67fee84af15b3
Alex <csit0st@gmx.de>
2018-11-07 18:28:51 +0100

    initial commit

---
+++ MikroservicesFrontEnd/DASConnection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+
+namespace MikroservicesFrontEnd
+{
+    class DASConnection
+    {
+
+        public static async Task<string> GetData()
+        {
+            HttpClient client = new HttpClient();
+            
+            Uri request = new Uri("http://www.contoso.com");
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            string responseString = "";
+
+            try
+            {
+                response = await client.GetAsync(request);
+                response.EnsureSuccessStatusCode();
+                responseString = await response.Content.ReadAsStringAsync();
+            } catch(Exception ex)
+            {
+                responseString = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
+            }
+
+
+            return responseString;
+        }
+
+        public void SendData()
+        {
+
+        }
+
+    }
+}
---
+++ MikroservicesFrontEnd/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
+
+namespace MikroservicesFrontEnd
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+        }
+
+        public void TestData()
+        {
+            Task<string> testt = DASConnection.GetData();
+            Debug.WriteLine("ANSWER:");
+            testt.Wait();
+            Debug.WriteLine(testt.Result);
+            Debug.WriteLine("END");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Task<string> testt = DASConnection.GetData();
+            Debug.WriteLine("ANSWER:");
+            testt.Wait();
+            Debug.WriteLine(testt.Result);
+            Debug.WriteLine("END");
+        }
+    }
+}
